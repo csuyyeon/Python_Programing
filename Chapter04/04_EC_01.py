59040da5417cd2f0e3d538a3482a08c49e230201
csuyyeon <124238983+csuyyeon@users.noreply.github.com>
2024-02-01 16:20:02 +0900

    Add files via upload

---
+++ Chapter04/04_EC_01.py
@@ -0,0 +1,5 @@
+cage=5
+fage=38-cage
+mage=34-cage
+print("아버지의 나이=",fage)
+print("어머니의 나이=",mage)
---
+++ Chapter04/04_EC_02.py
@@ -0,0 +1,5 @@
+radius=int(input("반지름:"))
+area=radius*radius*3.14
+circum=2*radius*3.14
+print("원의 넓이:",area)
+print("원의 둘레:",circum)
