94818154bb967f3c4cd3c5e9beee4d44ac062072
csuyyeon <124238983+csuyyeon@users.noreply.github.com>
2024-02-22 16:30:09 +0900

    Add files via upload

---
+++ 07_01.py
@@ -0,0 +1,21 @@
+print("12345678")
+print("홍길동")
+print("12345678")
+print("홍길동")
+print("")
+
+for i in range(2):
+    print("12345678")
+    print("홍길동")
+print("")
+
+def sn():
+    print("12345678")
+    print("홍길동")
+
+sn()
+sn()
+print("")
+
+for i in range(2):
+    sn()
